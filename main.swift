80b02b573e38bf5321fac25e921fdeb82fd7611b
Renan YFC <renanyfc07@gmail.com>
2025-07-06 02:01:03 -0300

    Chamar as funções da Aula 02

--- main.swift
+++ main.swift
@@ -9,3 +9,8 @@ import Foundation
 verificarPar(5)
 calcularFatorial(num: 5)
 verificarPrimo(num: 5)
+loopComNumerosPares(num: 12)
+numerosPrimosNaTabuada(num: 1)
+numerosParesEmFatoriais(num: 10)
+verificarSenha(senha: "Ab12##21bA")
+
